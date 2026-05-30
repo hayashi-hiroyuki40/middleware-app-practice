adf6e8e3542d339959bf028761e025a778f271bb
hayashi-hiroyuki <ai3672ght1@gmail.com>
2026-05-30 16:40:59 +0900

    first commit

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (! $request->user() || ! $request->user()->is_admin) {
+            abort(403,'管理者権限が必要です');}
+    return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,8 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 // 管理者ページ（ミドルウェア未適用 - 受講生が実装する）
-Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index'])->middleware('admin');
